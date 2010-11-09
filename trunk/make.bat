5e17f958610c0cab8d794192caee21b60bcddaa3
wicanr2 <wicanr2@07557586-8a0b-8cd6-2bd1-5dab5fce6798>
2010-11-09 16:18:44 +0000

    remove make batch file

--- trunk/make.bat
+++
@@ -1,4 +0,0 @@
-@Rem c:\Dev-cpp\bin\g++ -lstdc++ PipeReader.cpp test.cpp add.s -o test
-@Rem c:\Dev-cpp\bin\gcc --static -lwininet -lwsnmp32 -lwsock32 -lWs2_32 test2.c -o test2
-@Rem c:\Dev-cpp\bin\gcc test.c add.s -o test
-..\bin\make
