b9b03ae15cd37b852b48f9d9ed68e2b8f1ea1033
wicanr2 <wicanr2@07557586-8a0b-8cd6-2bd1-5dab5fce6798>
2010-12-06 02:59:45 +0000

    mmmmm

---
+++ trunk/os/part2/test.bat
@@ -0,0 +1,3 @@
+Rem qemu -L C:\os\qemu-0.9.0-windows\qemu-0.9.0-windows -hda hello.bin -boot c 
+qemu -L C:\os\qemu-0.9.0-windows\qemu-0.9.0-windows -fda hello.bin -boot a 
+qemu -L C:\os\qemu-0.9.0-windows\qemu-0.9.0-windows -fda hello2.bin -boot a 
